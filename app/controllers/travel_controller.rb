a923f489a3e6519d044ad9677e214a7418b92547
HansRCode <12411150+HansRCode@users.noreply.github.com>
2021-06-20 01:08:29 -0400

    Added weather

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -3,5 +3,50 @@ class TravelController < ApplicationController
   end
 
   def search
+
+    url = URI("https://restcountries-v1.p.rapidapi.com/name/#{params[:country]}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = 'fb582c8a59msh7ff2862d9aa0244p1e0ca7jsn83e3f796c4ca'
+    request["x-rapidapi-host"] = 'restcountries-v1.p.rapidapi.com'
+
+    response = http.request(request)
+
+    country = JSON.parse(response.body)
+
+    @country = country.first
+
+    @weather = find_weather(@country['capital'], @country['alpha2Code'])
+
+
   end
+
+
+  def find_weather(city, country_code)
+
+    url = URI("https://community-open-weather-map.p.rapidapi.com/weather?q=#{city}%2C#{country_code}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = 'fb582c8a59msh7ff2862d9aa0244p1e0ca7jsn83e3f796c4ca'
+    request["x-rapidapi-host"] = 'community-open-weather-map.p.rapidapi.com'
+
+    response = http.request(request)
+
+    return JSON.parse(response.body)
+
+    puts response.read_body
+       
+  end
+
+
 end
+
+
